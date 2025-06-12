b26c2da8037ededa47930f85bd484dc9783a2e14
João Paulo Pinheiro Mourão <joaopaulopinheiro59675@gmail.com>
2025-06-11 21:00:04 -0300

    feat: adding

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredUserJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyRecipeBook.Communication.Responses;
 
-namespace MyRecipeBook.Communication.Responses
+public class ResponseRegisteredUserJson
 {
-    public class ResponseRegisteredUserJson
-    {
-        public string Name { get; set; } = string.Empty;
-    }
+    public string Name { get; set; } = string.Empty;
 }
